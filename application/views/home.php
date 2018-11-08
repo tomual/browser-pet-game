73baeee4b6a2536e2e8a063ff0ddb632c3f293ec
tomual <tomual@users.noreply.github.com>
2018-11-08 18:39:39 +0800

    Fix missing links on home page

--- application/views/home.php
+++ application/views/home.php
@@ -3,9 +3,16 @@
 	
     <h2 class="my-5">Cocobox</h2>
 	<img src="<?php echo base_url('img/promo.png') ?>">
-	<h1>Adopt your pet today!</h1>
-	<p class="mb-4">Your island awaits! Cocobox is a social browser game. Dress your pet, interact with your pet, explore various maps, and more.</p>
-	<a href="" class="btn btn-primary btn-lg">Get my pet</a>
+	
+	<?php if ($this->user): ?>
+		<h1>Visit Your Island</h1>
+		<p class="mb-4">Your island awaits! Cocobox is a social browser game. Dress your pet, interact with your pet, explore various maps, and more.</p>
+		<a href="<?php echo base_url('world') ?>" class="btn btn-primary btn-lg">Visit island</a>
+	<?php else: ?>
+		<h1>Adopt your pet today!</h1>
+		<p class="mb-4">Your island awaits! Cocobox is a social browser game. Dress your pet, interact with your pet, explore various maps, and more.</p>
+		<a href="<?php echo base_url('user/signup') ?>" class="btn btn-primary btn-lg">Get my pet</a>
+	<?php endif ?>
 	<form method="post" action="<?php echo base_url('mailing') ?>" class="m-5 p-5">
 		<h6 class="text-center mb-3">Sign up for updates!</h6>
 		<div class="form-group mb-3">
